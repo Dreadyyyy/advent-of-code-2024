957912170583fe9e0fc6a0242ce798e4e210e6e6
Dreadyyyy <aboutegor8@gmail.com>
2024-12-05 19:22:35 +0300

    Improved 5.1 and 5.2

--- day5/task1.py
+++ day5/task1.py
@@ -1,23 +1,17 @@
-def check(rules: dict[int, list[int]], update: list[int]) -> bool:
-    prev = set()
-    for n in update:
-        if any(p in prev for p in rules.get(n, [])):
-            return False
-        prev.add(n)
-    return True
+from functools import cmp_to_key
 
 
 with open("input.txt") as f:
-    rules = {}
+    cmp = {}
     while (l := f.readline()) != "\n":
         n1, n2 = map(int, l.split("|"))
-        rules.setdefault(n1, [])
-        rules[n1].append(n2)
+        cmp[(n1, n2)] = -1
+        cmp[(n2, n1)] = 1
 
     updates = [list(map(int, l.split(","))) for l in f.readlines()]
 
-
-updates = [u for u in updates if check(rules, u)]
+key = cmp_to_key(lambda x, y: cmp[(x, y)])
+updates = [u for u in updates if u == sorted(u, key=key)]
 
 res = sum(u[len(u) // 2] for u in updates)
 print(res)
--- day5/task2.py
+++ day5/task2.py
@@ -1,31 +1,17 @@
 from functools import cmp_to_key
 
 
-def check(rules: dict[int, list[int]], update: list[int]) -> bool:
-    prev = set()
-    for n in update:
-        if any(p in prev for p in rules.get(n, [])):
-            return False
-        prev.add(n)
-    return True
-
-
 with open("input.txt") as f:
-    rules, cmp = {}, {}
+    cmp = {}
     while (l := f.readline()) != "\n":
         n1, n2 = map(int, l.split("|"))
-        rules.setdefault(n1, [])
-        rules[n1].append(n2)
         cmp[(n1, n2)] = -1
         cmp[(n2, n1)] = 1
 
     updates = [list(map(int, l.split(","))) for l in f.readlines()]
 
-updates = [
-    sorted(u, key=cmp_to_key(lambda x, y: cmp.get((x, y), x - y)))
-    for u in updates
-    if not check(rules, u)
-]
+key = cmp_to_key(lambda x, y: cmp[(x, y)])
+updates = [su for u in updates if u != (su := sorted(u, key=key))]
 
 res = sum(u[len(u) // 2] for u in updates)
 print(res)
